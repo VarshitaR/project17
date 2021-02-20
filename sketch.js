8ab1bc8cf58cf737cc75670aaff05f829c95b984
VarshitaR <75935310+VarshitaR@users.noreply.github.com>
2021-02-20 22:05:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+
+var monkey , monkey_running;
+var ground;
+var banana ,bananaImage, rock, rockImage;
+var bananaGroup, rockGroup;
+var score;
+var survivalTime = 0;
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png");
+  
+  bananaImage = loadImage("banana.png");
+  rockImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  
+  createCanvas(400,400);
+  
+ monkey = createSprite(80,315,20,20);
+ monkey.addAnimation("moving",monkey_running);
+ monkey.scale=0.1; 
+  
+ ground = createSprite(400,350,900,10);
+ ground.velocityX=-4;
+  
+ bananaGroup = createGroup();
+ rockGroup = createGroup();
+   
+}
+
+
+function draw() {
+
+  background(225);
+  
+  survivalTime = Math.ceil(frameCount/frameRate());
+  
+  textSize(20);
+  fill("black");
+  text("Survival Time : "+ survivalTime,100,50);
+  
+ if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+  
+ if (keyDown("space")){
+   
+      monkey.velocityY=-12;        
+ } 
+  
+  monkey.velocityY = monkey.velocityY + 0.8;
+  
+  monkey.collide(ground); 
+  
+  spawnBanana();
+  spawnRocks();
+  
+  drawSprites();
+  
+}
+
+function spawnBanana(){
+   if (frameCount % 80 === 0){
+     banana = createSprite(450,60,20,20);
+     banana.addImage(bananaImage);
+     banana.scale=0.1;
+     banana.y = Math.round(random(120,200));
+     banana.velocityX = -6;
+     banana.lifetime=200;
+     bananaGroup.add(banana);
+   }
+}
+
+function spawnRocks(){
+  if(frameCount % 300 === 0){
+    rocks = createSprite(450,327.5,10,40)
+    rocks.addImage(rockImage);
+    rocks.scale=0.1;
+    rocks.velocityX = -6; 
+    rocks.lifetime=200;
+    rockGroup.add(rocks);
+  }
+}
